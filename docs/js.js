e7940708172289e964b99d2d35743e2b91b8c945
olegrewko <olegrewko@gmail.com>
2022-09-23 17:11:47 +0300

    yu

---
+++ docs/js.js
@@ -0,0 +1,39 @@
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+  this.classList.add("active");
+});
+
+// var btn = document.getElementsByClassName("i__btn");
+// btn.addEventListener("click", function() {
+//   this.classList.add("active");
+// });
+
+
+function changeText () {
+  if (this.innerText == "В ИЗБРАННОЕ") { // check if text inside is "More"
+     this.innerText == "В ИЗБРАННОМ";    // If so, change to "Less"
+  } else {
+     this.innerText == "В ИЗБРАННОЕ";
+  }
+}
+
+/*
+Fetch the buttom element
+*/
+const button = document.body.querySelector('[data-target="#collapseExample"]');
+
+/*
+Add click event listener where we will provide logic that updates the button text
+*/
+button.addEventListener('click', function() {
+
+  /*
+  Update the text of the button to toggle beween "More" and "Less" when clicked
+  */
+  if(button.innerText.toLowerCase() === 'в избранное') {
+    button.innerText = 'В ИЗБРАННОМ';
+  }
+  else {
+    button.innerText = 'В ИЗБРАННОЕ';
+  }
+});
